43f86cbcd7907a2444961336ebdc6f6d02df15a0
Nicolasque <nicolasque1@gmail.com>
2018-04-13 00:05:59 +0200

    he hecho el egercicio de adivinar el numero

---
+++ grados_celsius_a_fahrenheit.py
@@ -0,0 +1,6 @@
+
+grados_celsius = int(input("cuantos grados celsius?:"))
+
+resultado = (grados_celsius * 1.8) + 32
+
+print("si hay {} grados celsius, hay {} grados fharenheit".format(grados_celsius, resultado))
