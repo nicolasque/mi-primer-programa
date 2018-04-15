510a48c61ec99586b5a119220ad784ef904f9fca
Nicolasque <nicolasque1@gmail.com>
2018-04-16 01:57:40 +0200

    he hecho el egercicio de adivinar el numero

--- media_de_varios_numeros.py
+++ media_de_varios_numeros.py
@@ -1,16 +1,20 @@
 
-numeros_usuario = [1]
+numeros_usuario = []
 numero_del_usuario = ""
 
-while len(numeros_usuario) > 5:
+while len(numeros_usuario) < 4:
     while not numero_del_usuario.isdigit():
         numero_del_usuario = input("Dime un numero: ")
     numeros_usuario.append(int(numero_del_usuario))
     numero_del_usuario = ""
     print("!Numero añadido¡")
 
-for numero in numeros_usuario:
-    suma_de_numeros = numero + numero
-dividisor = int(len(numeros_usuario))
+numero_grande = numeros_usuario[0]
 
-print("la media es {}".format(suma_de_numeros/dividisor))
+suma_de_numeros = numeros_usuario[0] +numeros_usuario[1] +numeros_usuario[2] +numeros_usuario[3]
+
+divisor = len(numeros_usuario)
+
+resultado = suma_de_numeros / divisor
+
+print("El numero mas pequeño es {}".format(resultado))
