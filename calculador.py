e83387d2de0d83e1b735174c3e60e7011c75ef2a
Nicolasque <nicolasque1@gmail.com>
2018-04-15 03:26:07 +0200

    he hecho el egercicio de adivinar el numero

--- calculador.py
+++ calculador.py
@@ -4,7 +4,7 @@ primer_numero = int(input("elege el primer numero:"))
 segundo_numero = int(input("elige el segundo numero:"))
 
 if operacion_elegila == "SUMA":
-    resulatado  = primer_numero + segundo_numero
+    resulatado = primer_numero + segundo_numero
 
 elif operacion_elegila == "RESTA":
     resulatado = primer_numero - segundo_numero
@@ -22,4 +22,4 @@ elif operacion_elegila == "RAIZ":
     import math
     resulatado = math.sqrt(primer_numero)
 
-print("el resultado de la {} es: {}".format(operacion_elegila, resulatado))
+print("el resultado de la {} es: {}".format(operacion_elegila,resulatado))
